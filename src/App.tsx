1dc98c264886af6657e1635448f789f715bfc79a
praniil <pparajuli15@gmail.com>
2024-02-02 14:48:27 +0545

    feat: basic structure of the app made

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Frontend/navbar/Navbar';
+// import Navbar from "./Frontend/"
+import Home from "./Frontend/HomePage"
 import './App.css';
+import ChatWindow from './Frontend/chatWindow/ChatWindow';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes> 
+        <Route path="/" element={<Home/>}/>
+        <Route path="/chatwindow" element={<ChatWindow/>}/>
+        <Route path="/"/>
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
---
+++ src/Frontend/chatWindow/ChatWindow.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { useReducer } from 'react'
+
+enum ActionType {
+  AddMessage = 'ADD_MESSAGE',
+  SetMessage = 'SET_MESSAGE',
+}
+
+interface AddMessageAction {
+  type: ActionType.AddMessage;
+  payload: string
+}
+
+interface SetMessageAction {
+  type: ActionType.SetMessage;
+  payload: string
+}
+
+type Action = AddMessageAction | SetMessageAction;
+
+interface initialStateInterface {
+  message: string;
+  messages: string[];
+}
+const initialState: initialStateInterface = {
+  message: '',
+  messages: [],
+}
+
+function reducer(state: initialStateInterface, action: Action) {
+  switch (action.type) {
+    case ActionType.AddMessage:
+      return { ...state, messages: [...state.messages, action.payload] }
+    case ActionType.SetMessage:
+      return { ...state, message: action.payload }
+    default:
+      return state
+  }
+}
+
+const ChatWindow: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({type:ActionType.SetMessage, payload: event.target.value})
+  }
+  function handleSendButton() {
+    if (state.message.trim() != '') {         //checks whether the message is empty
+      dispatch({type:ActionType.AddMessage, payload:state.message})
+      dispatch({type:ActionType.SetMessage, payload:''})
+    }
+  }
+  function handleKeyPress(event:React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key == 'Enter') {
+      handleSendButton();
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        {state.messages.map((message, index) => (
+          <div key={index}>
+            {message}
+          </div>
+        ))}
+      </div>
+      <input type="text" name="message" id="message" placeholder='write your message' value={state.message} onChange={handleMessageChange} onKeyDown={handleKeyPress}/>
+      <button onClick={handleSendButton}>Send</button>
+    </div>
+  )
+}
+
+export default ChatWindow
